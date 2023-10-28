cb5907bd1a03cf6411aa7373489667ca131473dc
KENTARO OKA <kentarooka@macbook-pro.home>
2023-10-28 18:37:27 -0400

    Modifying terraform code for EKS module and SG to support to infra

--- terraform_code/vpc/V5-eks_setup.tf
+++ terraform_code/vpc/V5-eks_setup.tf
@@ -9,10 +9,10 @@ resource "aws_instance" "demo-server" {
     //security_groups = [ "demo-sg" ]
     vpc_security_group_ids = [aws_security_group.demo-sg.id]
     subnet_id = aws_subnet.dpp-public-subnet-01.id 
-for_each = toset(["jenkins-master", "build-slave", "ansible"])
-   tags = {
-     Name = "${each.key}"
-   }
+    for_each = toset(["jenkins-master", "build-slave", "ansible"])
+    tags = {
+      Name = "${each.key}"
+    }
 }
 
 resource "aws_security_group" "demo-sg" {
@@ -103,14 +103,14 @@ resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
   route_table_id = aws_route_table.dpp-public-rt.id   
 }
 
-  module "sgs" {
-    source = "../sg_eks"
-    vpc_id     =     aws_vpc.dpp-vpc.id
-  }
+module "sgs" {
+  source = "../sg_eks"
+  vpc_id     =     aws_vpc.dpp-vpc.id
+}
 
-  module "eks" {
-      source = "../eks"
-       vpc_id     =     aws_vpc.dpp-vpc.id
-       subnet_ids = [aws_subnet.dpp-public-subnet-01.id,aws_subnet.dpp-public-subnet-02.id]
-      sg_ids = module.sgs.security_group_public
-  }
+module "eks" {
+    source = "../eks"
+      vpc_id     =     aws_vpc.dpp-vpc.id
+      subnet_ids = [aws_subnet.dpp-public-subnet-01.id,aws_subnet.dpp-public-subnet-02.id]
+    sg_ids = module.sgs.security_group_public
+}
